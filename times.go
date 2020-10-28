96db6141642b6022f3c2abfcda3bf510cba4e5e5
xgh2012 <55821284@qq.com>
2020-10-28 14:24:14 +0800

    返回当前月份的第一天0点 最后一天23点时间

--- times.go
+++ times.go
@@ -34,3 +34,20 @@ func transferTimeFormatString(format string) string {
 	}
 	return format
 }
+
+//获取传入的时间所在月份的第一天，即某月第一天的0点。如传入time.Now(), 返回当前月份的第一天0点时间。
+func GetFirstDateOfMonth(d time.Time) time.Time {
+	d = d.AddDate(0, 0, -d.Day()+1)
+	return GetZeroTime(d)
+}
+
+//获取传入的时间所在月份的最后一天，即某月最后一天的0点。如传入time.Now(), 返回当前月份的最后一天0点时间。
+func GetLastDateOfMonth(d time.Time) time.Time {
+	return GetFirstDateOfMonth(d).AddDate(0, 1, -1)
+}
+
+//获取某一天的0点时间
+func GetZeroTime(d time.Time) time.Time {
+	t := time.Date(d.Year(), d.Month(), d.Day(), 0, 0, 0, 0, d.Location())
+	return t
+}
